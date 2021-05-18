3661c57b29ddc42a62985bdc593c89e48b406ce5
meroa006 <merouani.ass@gmail.com>
2021-05-18 12:55:00 -0400

    These are my changes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(" Hello from GitHub");
+        System.out.println(" Hello from GitHub!");
+        var person = new Person();
     }
 }
